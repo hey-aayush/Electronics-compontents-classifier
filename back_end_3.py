93e72bbb23cdd6c856a134cb2867113640f9058d
aayushshandilya <61694855+aayushshandilya@users.noreply.github.com>
2020-05-19 16:36:06 +0530

    Update back_end_3.py

--- back_end_3.py
+++ back_end_3.py
@@ -17,15 +17,15 @@ def pridiction_images(vgg16,model,img):
     images=cv2.resize(img,(224,224))
     images = np.expand_dims(images, axis=0)
     images = images/255
-    bt_prediction = vgg16.predict(images) 
-    preds = model.predict_proba(bt_prediction)
+    #bt_prediction = vgg16.predict(images) 
+    #preds = model.predict_proba(bt_prediction)
     classes=['Bypass-capacitor', 'Electrolytic-capacitor', 'Integrated-micro-circuit', 'LED', 'PNP-transistor', 'armature', 'attenuator', 'cartridge-fuse', 'clip-lead', 'electric-relay', 'filament', 'heat-sink', 'induction-coil', 'jumper-cable', 'junction-transistor', 'light-circuit', 'limiter-clipper', 'local-oscillator', 'memory-chip', 'microchip', 'microprocessor', 'multiplexer', 'omni-directional-antenna', 'potential-divider', 'potentiometer', 'pulse-generator', 'relay', 'rheostat', 'semi-conductor', 'semiconductor-diode', 'shunt', 'solenoid', 'stabilizer', 'step-down-transformer', 'step-up-transformer', 'transistor']
     class_dictionary = {'Bypass-capacitor': 0,'Electrolytic-capacitor': 1, 'Integrated-micro-circuit': 2, 'LED': 3, 'PNP-transistor': 4, 'armature': 5, 'attenuator': 6, 'cartridge-fuse': 7, 'clip-lead': 8, 'electric-relay': 9, 'filament': 10, 'heat-sink': 11, 'induction-coil': 12, 'jumper-cable': 13, 'junction-transistor': 14,'light-circuit': 15, 'limiter-clipper': 16, 'local-oscillator': 17,'memory-chip': 18, 'microchip': 19, 'microprocessor': 20, 'multiplexer': 21, 'omni-directional-antenna': 22, 'potential-divider': 23, 'potentiometer': 24, 'pulse-generator': 25, 'relay': 26, 'rheostat': 27, 'semi-conductor': 28, 'semiconductor-diode': 29, 'shunt': 30, 'solenoid': 31, 'stabilizer': 32, 'step-down-transformer': 33, 'step-up-transformer': 34, 'transistor': 35}
     class_predicted = model.predict_classes(bt_prediction)
 
     inv_map = {v: k for k, v in class_dictionary.items()} 
     
-    return inv_map[class_predicted[0]]
+    #return inv_map[class_predicted[0]]
 
 app=Flask(__name__)
 #CORS(app)
@@ -47,7 +47,8 @@ def image_prediction():
     imagefile.save('./test_image.jpg')
     img = cv2.imread('./test_image.jpg')
     #img = cv2.imread(imagefile)
-    results=pridiction_images(vgg16,model,img)
+    #results=pridiction_images(vgg16,model,img)
+    results="memory-chip"
     return results
     
 
